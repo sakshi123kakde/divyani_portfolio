cee932c3e368f4183d9f4182455407b0cc0ca4fd
sakshi Kakde <sakshi.kakde2004@gmail.com>
2026-02-13 22:30:54 +0530

    Fix router for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
 import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <App />
   </HashRouter>
